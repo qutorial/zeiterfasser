e15dca94876a579bc31c3ad1cbe1b8d6c71dee0a
Zaur <zaur@genua.de>
2018-01-08 20:40:51 +0100

    Token auth installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108152241) do
+ActiveRecord::Schema.define(version: 20180108192127) do
 
   create_table "eintrags", force: :cascade do |t|
     t.integer "kosten_traeger_id"
@@ -48,8 +48,12 @@ ActiveRecord::Schema.define(version: 20180108152241) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.text "tokens"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
