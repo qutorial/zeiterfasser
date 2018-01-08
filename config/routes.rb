dadc418ec0eb8912cc9c8cf5e25cc34dfca71f7a
Zaur <zaur@genua.de>
2018-01-08 21:13:49 +0100

    Auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,17 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   authenticate :user do
-   # resources :something do
-   #   resources :something_embedded, only: [:new, :create, :destroy, :edit, :update]
-   # end
-  end
-
-  resources :eintrags
-  resources :kosten_traegers
-
-  scope '/api' do
-    scope '/v1' do
-          resources :kosten_traegers
-      scope '/eintrags' do
-        post 'atonce', to: 'at_once#atonce'
+    scope '/api' do
+      scope '/v1' do
+        resources :kosten_traegers
+        resources :eintrags
+        scope '/eintrags' do
+          post 'atonce', to: 'at_once#atonce'
+        end
       end
     end
-  end
-  
-  root to: 'static#home'
+  end  
 
-  get 'noscript', to: 'static#noscript'
+  root to: 'static#home'
   
 end
